e2692e008920640f03f413931c06686e55351244
elwood-berry <eberry@anterasoftware.com>
2021-08-05 17:00:13 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,16 +23,106 @@ export interface PeriodicElement {
 
 //
 const ELEMENT_DATA: PeriodicElement[] = [
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
+  {
+    firstName: 'Al',
+    lastName: 'Pacino',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 1,
+    name: 'Hydrogen',
+    weight: 1.0079,
+    symbol: 'H'
+  },
+  {
+    firstName: 'Tony',
+    lastName: 'Montana',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 2,
+    name: 'Helium',
+    weight: 4.0026,
+    symbol: 'He'
+  },
+  {
+    firstName: 'Michelle',
+    lastName: 'Pfeiffer',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 3,
+    name: 'Lithium',
+    weight: 6.941,
+    symbol: 'Li'
+  },
+  {
+    firstName: 'Elvira',
+    lastName: 'Jones',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 4,
+    name: 'Beryllium',
+    weight: 9.0122,
+    symbol: 'Be'
+  },
+  {
+    firstName: 'Steven',
+    lastName: 'Bauer',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 5,
+    name: 'Boron',
+    weight: 10.811,
+    symbol: 'B'
+  },
+  {
+    firstName: 'Manny',
+    lastName: 'Ribera',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 6,
+    name: 'Carbon',
+    weight: 12.0107,
+    symbol: 'C'
+  },
+  {
+    firstName: 'Mary',
+    lastName: 'Mastrantonio',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 7,
+    name: 'Nitrogen',
+    weight: 14.0067,
+    symbol: 'N'
+  },
+  {
+    firstName: 'Gina',
+    lastName: 'Lewis',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 8,
+    name: 'Oxygen',
+    weight: 15.9994,
+    symbol: 'O'
+  },
+  {
+    firstName: 'Robert',
+    lastName: 'Last Name',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 9,
+    name: 'Fluorine',
+    weight: 18.9984,
+    symbol: 'F'
+  },
+  {
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    email: 'email@gmail.com',
+    phone: '(000) 000-0000',
+    position: 10,
+    name: 'Neon',
+    weight: 20.1797,
+    symbol: 'Ne'
+  }
 ];
 
 //
@@ -54,6 +144,11 @@ export class AppComponent {
 
     //
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  // SHOW DETAILS
+  public showDetails(data) {
+    console.log('You have clicked on Me....', data);
   }
 }
 
