826760563b9339999a75c97631f8bb2bb19ff905
elwood-berry <eberry@anterasoftware.com>
2021-08-05 14:17:42 -0500

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,57 @@ This code is brought to you by Elwood Berry @www.elwoodberry.com
 */
 
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+// INTERFACE
+export interface PeriodicElement {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+
+  // ---
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+//
+const ELEMENT_DATA: PeriodicElement[] = [
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { firstName: "First Name", lastName: "Last Name", email: "email@gmail.com", phone: "(000) 000-0000", position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
+];
+
+//
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  // PROPERTIES
+  // public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  public displayedColumns: string[] = ['position'];
+  public dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  //
+  applyFilter(event: Event) {
+    //
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    //
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
 
 /*
 -------------------------------------------------
